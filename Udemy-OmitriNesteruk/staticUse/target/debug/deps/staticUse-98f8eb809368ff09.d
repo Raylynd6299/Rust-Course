7c86924f30a3d4c186f81c52d5a3b1976743e2c1
Raylynd6299 <ray_escom@outlook.com>
2020-09-23 19:17:00 -0500

    deb

---
+++ Udemy-OmitriNesteruk/staticUse/target/debug/deps/staticUse-98f8eb809368ff09.d
@@ -0,0 +1,5 @@
+/home/ray/Desktop/Learning/Rust_Course/Udemy-OmitriNesteruk/staticUse/target/debug/deps/staticUse-98f8eb809368ff09.rmeta: src/main.rs
+
+/home/ray/Desktop/Learning/Rust_Course/Udemy-OmitriNesteruk/staticUse/target/debug/deps/staticUse-98f8eb809368ff09.d: src/main.rs
+
+src/main.rs:
